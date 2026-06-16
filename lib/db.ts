9187ce0d5ce88fb902489bfa999402ff76124e08
Paulo Jimena <engrpaulojimena@gmail.com>
2026-06-16 17:06:20 +0800

    Update project

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,8 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "student_registration_db",
-  password: "1",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required para sa Neon
+  },
 });
